4e0e9c3d9bccdbc1337d53939dd1da1968786c99
Francescogiovan8 <francescogiovane8@gmail.com>
2026-07-02 18:51:34 +0200

    Completato il flusso interattivo del MainTest

--- MainTest.java
+++ MainTest.java
@@ -1,5 +1,6 @@
 import data.Data;
 import tree.RegressionTree;
+import utility.Keyboard;
 
 import data.TrainingDataException;
 import tree.UnknownValueException;
@@ -12,12 +13,19 @@ class MainTest {
 	public static void main(String[] args){
 		Data trainingSet;
 
-		try{
-			trainingSet= new Data("servo.dat");
+		System.out.println("Training set:");
+		String fileName = Keyboard.readString();
+
+		System.out.println("Starting data acquisition phase!");
+
+		try {
+			trainingSet = new Data(fileName);
 		} catch (TrainingDataException e) {
 			System.out.println(e);
 			return;
 		}
+
+		System.out.println("Starting learning phase!");
 
 		RegressionTree tree=new RegressionTree(trainingSet);
 		
@@ -25,10 +33,20 @@ class MainTest {
 		
 		tree.printTree();
 		
-		try {
-			System.out.println("Predicted class: " + tree.predictClass());
-		} catch (UnknownValueException e) {
-			System.out.println(e);
-		}
+		char answer;
+
+		do {
+			System.out.println("Starting prediction phase!");
+
+			try {
+				System.out.println("Predicted class: " + tree.predictClass());
+			} catch (UnknownValueException e) {
+				System.out.println(e);
+			}
+
+			System.out.println("Would you repeat ? (y/n)");
+			answer = Keyboard.readChar();
+
+		} while (answer == 'y');
 	}
 }
